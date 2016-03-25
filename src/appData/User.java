c80ed195ac8198c40f2cf751e122284da9b00e82
me0864 <leliu08640@gmail.com>
2016-03-24 21:17:55 -0400

    Some changes to User

--- src/appData/User.java
+++ src/appData/User.java
@@ -4,23 +4,57 @@ import java.util.ArrayList;
 
 public class User implements java.io.Serializable {
 
-	String username;
-	ArrayList<Album> listofAlbums;
-	ArrayList<Photo> photosinAlbum;
-	
-	public User(){
-		
-	}
-	
-	public User(String username){
-		this.username = username;
-		listofAlbums = null;
-		photosinAlbum = null;
-	}
-	
-	public String toString(){
-		return username;
-	}
-	
-	
+    String username;
+    ArrayList<Album> listofAlbums;
+    ArrayList<Photo> photosinAlbum;
+
+    public User() {
+
+    }
+
+    public User(String username) {
+        this.username = username;
+        listofAlbums = null;
+        photosinAlbum = null;
+    }
+
+    public String toString() {
+        return username;
+    }
+
+    /* 
+    Fetch album of user
+        User x = new User(Bob);
+        ArrayList arr = new ArrayList();
+        arr=x.ArrayList();  
+    */
+    public ArrayList<Album> userAlbum() {
+        return listofAlbums;
+    }
+    
+    /*
+    Fetch photos of user
+    */
+    public ArrayList<Photo> userPhotos() {
+        return photosinAlbum;
+    }
 }
+
+    /*
+    Serialization of album
+        Move to where we want the serialization to happen.
+        - screen transition?
+        - or just any changes to album get instantly serialized?
+    */
+//      try
+//      {
+//         FileOutputStream albumListOut = new FileOutputStream("/albumList.ser");
+//         ObjectOutputStream out = new ObjectOutputStream(albumListOut);
+//         out.writeObject(listofAlbums);
+//         out.close();
+//         albumListOut.close();
+//         System.out.printf("Serialized data is saved in /albumList.ser");
+//      }catch(IOException i)
+//      {
+//          i.printStackTrace();
+//      }
