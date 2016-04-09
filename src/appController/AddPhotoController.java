29b9f81438d98edc812b3d903208cf08c9250607
me0864 <leliu08640@gmail.com>
2016-04-09 11:05:06 -0400

    added some id

--- src/appController/AddPhotoController.java
+++ src/appController/AddPhotoController.java
@@ -1,14 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package appController;
 
-/**
- *
- * @author le
- */
+import java.io.FileNotFoundException;
+
+import appData.Admin;
+import appData.User;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+
 public class AddPhotoController {
-    
+        @FXML
+	Button addPhotoButton;
+	@FXML
+	TextField photoUrlField;
+        @FXML
+	TextField captionField;
+        @FXML
+	TextField tagField;
+        
+        public void addPhoto(ActionEvent event) throws Exception {
+            
+	}
 }
